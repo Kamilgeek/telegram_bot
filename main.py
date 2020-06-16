3ea9a31e72ad95e3fa1bbec637e630d262bc45a1
Kamilgeek <kamil-magomedov-00@mail.ru>
2020-06-16 23:58:36 +0400

    did somethink2

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
 bot.polling()
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text == 'Привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text == 'Пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
