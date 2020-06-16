09c79d72cc81aea07e55321fd905539d56406484
Kamilgeek <kamil-magomedov-00@mail.ru>
2020-06-16 23:56:41 +0400

    did somethink2

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import telebot
 bot = telebot.TeleBot('ваш токен')
 @bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+bot.polling()
